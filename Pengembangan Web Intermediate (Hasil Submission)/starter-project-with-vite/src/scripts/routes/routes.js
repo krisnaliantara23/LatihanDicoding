4fb564cef8f45a1386c5ec678be5c940c6163b91
krisnaliantara23 <krisnaliantara2333@gmail.com>
2025-06-13 23:30:31 +0800

    Perubahan bagian ini

--- Pengembangan Web Intermediate (Hasil Submission)/starter-project-with-vite/src/scripts/routes/routes.js
+++ Pengembangan Web Intermediate (Hasil Submission)/starter-project-with-vite/src/scripts/routes/routes.js
@@ -2,12 +2,14 @@ import HomePage from '../pages/home/home-page';
 import AboutPage from '../pages/about/about-page';
 import AddPage from '../pages/add/add-page';
 import LoginPage from '../pages/login/login-page'; // Import halaman Login
+import RegisterPage from '../pages/register/register-page';
 
 const routes = {
   '/': new HomePage(),
   '/about': new AboutPage(),
   '/add': new AddPage(),
-  '/login': new LoginPage(), // Rute untuk Login
+  '/login': new LoginPage(),
+  '/register': new RegisterPage(), // Tambahkan rute untuk halaman Register
 };
 
 export default routes;
